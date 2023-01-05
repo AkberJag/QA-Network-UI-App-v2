e0b9fbd9e3188c071a117f7aabfe4d6e64af16dd
Akber-MAC <akberjag@gmail.com>
2023-01-05 19:09:24 +0530

    refactor

--- networkui/ipaddress/views.py
+++ networkui/ipaddress/views.py
@@ -1,7 +1,7 @@
 """ IP address views."""
 
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, flash
 from networkui.config import is_a_script_running
 
 blueprint = Blueprint(
@@ -15,4 +15,6 @@ blueprint = Blueprint(
 @blueprint.route("/add", methods=["GET", "POST"])
 def add():
     """Add new ip address"""
+
+    flash("this is a test flash", "danger")
     return render_template("ipaddress/add_ip_address.html")
